2b8e9c6772919d790de1cb3db97edb080b2ccefd
Svovoniks <svovoniks@gmail.com>
2024-10-08 08:02:17 +0300

    fix 2

--- 02/main.go
+++ 02/main.go
@@ -7,17 +7,17 @@ import (
 
 func PrintSquare(num int, wg *sync.WaitGroup) {
 	fmt.Println(num * num)
-    wg.Done()
+	wg.Done()
 }
 
 func main() {
-    arr := [...]int{2,4,6,8,10}
-    var wg sync.WaitGroup
+	arr := [...]int{2, 4, 6, 8, 10}
+	var wg sync.WaitGroup
 
-    for _, i := range arr {
-        wg.Add(1)
-        go PrintSquare(i, &wg)
-    }
+	for _, i := range arr {
+		wg.Add(1)
+		go PrintSquare(i, &wg)
+	}
 
-    wg.Wait()
+	wg.Wait()
 }

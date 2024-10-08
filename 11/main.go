4b53abbe0f57f8224b21a02ca1d2b94d50472600
Svovoniks <svovoniks@gmail.com>
2024-10-08 20:59:32 +0300

    fix 11

--- 11/main.go
+++ 11/main.go
@@ -2,15 +2,19 @@ package main
 
 import "fmt"
 
-func intersect(arr1 *[]int, arr2 *[]int) []int {
+func intersect(arr1 []int, arr2 []int) []int {
 	mp := make(map[int]bool)
 	var res []int
 
-	for _, i := range *arr1 {
+	if len(arr1) > len(arr2) {
+		arr1, arr2 = arr2, arr1
+	}
+
+	for _, i := range arr1 {
 		mp[i] = true
 	}
 
-	for _, i := range *arr2 {
+	for _, i := range arr2 {
 		if mp[i] {
 			res = append(res, i)
 		}
@@ -21,8 +25,8 @@ func intersect(arr1 *[]int, arr2 *[]int) []int {
 }
 
 func main() {
-	set1 := []int{20, 2, 3, 4, 5}
-	set2 := []int{2, 3, 5, 7, 8, 9, 20}
+	set1 := []int{20, 2, 3, 4, 5, 100, 1000, 710}
+	set2 := []int{200, 33, 5, 710, 8, 9, 20}
 
-	fmt.Println(intersect(&set1, &set2))
+	fmt.Println(intersect(set1, set2))
 }
